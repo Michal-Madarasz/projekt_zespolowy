6ca5ae787783aec5a120cd629f1927d19525628c
Mariusz-Magiera <235860@student.pwr.edu.pl>
2019-03-24 15:24:03 +0100

    Dodano listowanie poszkodowanych, podgląd dokładnego stanu każdego poszkodowanego i tymczasową losową generację poszkodowanych

--- coordinator_app/app/src/main/java/com/example/coordinator_app/MainActivity.java
+++ coordinator_app/app/src/main/java/com/example/coordinator_app/MainActivity.java
@@ -1,17 +1,31 @@
 package com.example.coordinator_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.Layout;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 import android.widget.ViewFlipper;
+import java.util.ArrayList;
+import java.util.Random;
+
+/*
+TODO: panel zarządzania ratownikami
+TODO: obsługa bluetooth
+TODO: panel podsumowujący poszkodowanych
+ */
 
 public class MainActivity extends AppCompatActivity {
+
+    Victim asd = new Victim(1L, true, 20, 1.0f, true, Victim.AVPU.AWAKE);
+    ArrayList<Victim> victims = new ArrayList<>();
+    CustomAdapter customAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +34,35 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        final ListView victimList = findViewById(R.id.victim_list);
+        victims.add(asd);
+        customAdapter = new CustomAdapter(getApplicationContext(), victims);
+        victimList.setAdapter(customAdapter);
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Toast.makeText(getApplicationContext(), "Losowa generacja pacjenta", Toast.LENGTH_LONG ).show();
+                Random r = new Random();
+                long imei = r.nextLong()%1000000000000000L;
+                boolean b = r.nextBoolean();
+                int rate = r.nextInt(40) + 10;
+                float capRefillTime = r.nextFloat()*3f + 0.5f;
+                boolean w = r.nextBoolean();
+                Victim.AVPU c = Victim.AVPU.values()[r.nextInt(Victim.AVPU.values().length)];
+                Victim randomVictim = new Victim(imei, b, rate, capRefillTime, w, c);
+                victims.add(randomVictim);
+                customAdapter.notifyDataSetChanged();
+            }
+        });
+
+        victimList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), VictimDetailsActivity.class);
+                intent.putExtra("victim", victims.get(position));
+                startActivity(intent);
             }
         });
     }
